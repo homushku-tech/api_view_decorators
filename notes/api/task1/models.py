9cf42f8de5db769ae3018cb7631aa279b8ea0a4c
Andrew <nhomusku452@gmail.com>
2025-08-17 19:10:29 +0300

    initial commit

---
+++ notes/api/task1/models.py
@@ -0,0 +1,15 @@
+
+
+from django.db import models
+
+class Note(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'note'
+        
+    def __str__(self):
+        return self.title
---
+++ notes/api/task1/urls.py
@@ -0,0 +1,9 @@
+
+
+from django.urls import path
+from .views import get_notes
+
+
+urlpatterns = [
+    path('notes/', get_notes),
+]
